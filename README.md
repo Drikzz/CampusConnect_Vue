d6b330bac64c3ae8b53a0f9d17213ed4e2c25cd8
drikzz <rikzsuarez@gmail.com>
2024-10-31 18:15:43 +0800

    try readme
    adasd

--- README.md
+++ README.md
@@ -1,66 +1,69 @@
-<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
+# A Guide to cloning a Laravel Repository
 
-<p align="center">
-<a href="https://github.com/laravel/framework/actions"><img src="https://github.com/laravel/framework/workflows/tests/badge.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
-</p>
+- __1st__, check if your current branch is the same as main branch itself.
+- __2nd__, Clone the current branch to your local storage and open it in VS Code.
 
-## About Laravel
+# WARNING
+Please refer to <a href="https://youtu.be/XTDNs4TB_lE?si=sb2QOxhU0OEvEGPX">Installation of Laravel</a> for the installation of the following:
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+- Composer
+- XAMPP
+- Laravel
 
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+If you already installed those, it's better to it check your sytem.
 
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
+- __3rd__, open CMD and type the following commands:
 
-## Learning Laravel
+To check for composer:
+```
+composer -v
+```
 
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
+To check for Laravel:
+```
+laravel
+```
 
-You may also try the [Laravel Bootcamp](https://bootcamp.laravel.com), where you will be guided through building a modern Laravel application from scratch.
+- __4th__, go now to the cloned repository and do the following commands.
 
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains thousands of video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
+- __5th__, open the terminal in your VSCODE using 'CTRL + SHIFT + `'.
 
-## Laravel Sponsors
+- __6th__, copy the *.env.example* to create a *.env* using:
+```
+cp .env.example .env
+```
 
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the [Laravel Partners program](https://partners.laravel.com).
+- __7th__, open the 'env' and locate the following:
+```
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=cc_website
+DB_USERNAME=root
+DB_PASSWORD=
+```
+- You can change the 'DB_DATABASE=cc_website' to your own database name:
 
-### Premium Partners
+- __8th__, run the 'php artisan key:generate' command to generate new key:
+```
+php artisan key:generate
+```
 
-- **[Vehikl](https://vehikl.com/)**
-- **[Tighten Co.](https://tighten.co)**
-- **[WebReinvent](https://webreinvent.com/)**
-- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
-- **[64 Robots](https://64robots.com)**
-- **[Curotec](https://www.curotec.com/services/technologies/laravel/)**
-- **[Cyber-Duck](https://cyber-duck.co.uk)**
-- **[DevSquad](https://devsquad.com/hire-laravel-developers)**
-- **[Jump24](https://jump24.co.uk)**
-- **[Redberry](https://redberry.international/laravel/)**
-- **[Active Logic](https://activelogic.com)**
-- **[byte5](https://byte5.de)**
-- **[OP.GG](https://op.gg)**
+- __9th__, after all, that make sure to run your XAMPP.
 
-## Contributing
+- __10th__, run the 'php artisan migrate' command to run the migrations in the clone project.
+```
+php artisan migrate
+```
 
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
+- __11th__, run 'php artisan serve' command and go to the link provided:
 
-## Code of Conduct
+```
+php artisan serve
+```
 
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
+- __12th__, run 'npm run dev' command:
 
-## Security Vulnerabilities
-
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
-
-## License
-
-The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+```
+npm run dev
+```
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -34,26 +34,31 @@
             </div>
             
             {{-- for searching --}}
-            <form class="relative inline-flex items-center">
+            <form action="" method="post">
+
                 @csrf
-                <input class="py-2 pl-10 pr-4 rounded-full w-[20rem] text-sm" type="text" name="search" id="search" placeholder="Search for Products..." value=" {{ old('search') }}">
-                <button class="absolute left-4" type="submit">
-                    <svg class="w-4 h-auto" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
+
+                <div class="relative flex items-center fill-slate-500 focus-within:fill-black transition-colors">
+                    <svg class="w-5 h-5 absolute ml-3 " xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
                         <path d="M23.707,22.293l-5.969-5.969a10.016,10.016,0,1,0-1.414,1.414l5.969,5.969a1,1,0,0,0,1.414-1.414ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z"/>
-                    </svg> 
-                </button>
+                    </svg>
+                    
+                    <input type="text" name="search-text" id="search-text" autocomplete="off" placeholder="Search for products.." class="pl-10 pr-3 px-3 py-2 w-[20rem] rounded-full text-black border-none focus:ring-2 focus:ring-black focus:outline-none font-Satoshi">
+                    
+                </div>
+
             </form>
 
             <div class="flex justify-between items-center w-[6rem]">
-                <svg class="w-4 h-auto fill-white" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
+                <svg class="w-5 h-auto fill-white" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
                     <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z"/>
                 </svg>
 
-                <svg class="w-4 h-auto fill-white" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
+                <svg class="w-5 h-auto fill-white" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
                     <path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z"/><path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z"/>
                 </svg>
 
-                <svg class="w-4 h-auto fill-white" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512">
+                <svg class="w-5 h-auto fill-white" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512">
                     <path d="m19,4h-1.101c-.465-2.279-2.485-4-4.899-4H5C2.243,0,0,2.243,0,5v12.854c0,.794.435,1.52,1.134,1.894.318.171.667.255,1.015.255.416,0,.831-.121,1.19-.36l2.95-1.967c.691,1.935,2.541,3.324,4.711,3.324h5.697l3.964,2.643c.36.24.774.361,1.19.361.348,0,.696-.085,1.015-.256.7-.374,1.134-1.1,1.134-1.894v-12.854c0-2.757-2.243-5-5-5ZM2.23,17.979c-.019.012-.075.048-.152.007-.079-.042-.079-.109-.079-.131V5c0-1.654,1.346-3,3-3h8c1.654,0,3,1.346,3,3v7c0,1.654-1.346,3-3,3h-6c-.327,0-.541.159-.565.175l-4.205,2.804Zm19.77,3.876c0,.021,0,.089-.079.131-.079.041-.133.005-.151-.007l-4.215-2.811c-.164-.109-.357-.168-.555-.168h-6c-1.304,0-2.415-.836-2.828-2h4.828c2.757,0,5-2.243,5-5v-6h1c1.654,0,3,1.346,3,3v12.854Z"/>
                 </svg>
             </div>
@@ -65,33 +70,12 @@
     </main>
 
     {{-- finish footer tomorrow --}}
-    <footer class="w-full">
-        {{-- main container footer --}}
-        <div class="flex flex-col justify-center w-full">
-            <div class="flex justify-center items-center max-w-screen-xl bg-black py-10">
-                <div class="flex justify-center items-center">
-                    <p class="font-Footer italic w-[14rem] text-white">
-                        STAY UP TO DATE ABOUT OUR LATEST OFFERS
-                    </p>
-                </div>
+    <footer class="w-full h-5 bg-slate-600">
+        <div>
+            
+        </div>
+        <div>
 
-                <div class="flex justify-center items-center">
-                    <form class="relative inline-flex items-center">
-                        <svg class="w-4 h-auto absolute left-4 stroke-gray-50" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
-                            <path d="M19,1H5A5.006,5.006,0,0,0,0,6V18a5.006,5.006,0,0,0,5,5H19a5.006,5.006,0,0,0,5-5V6A5.006,5.006,0,0,0,19,1ZM5,3H19a3,3,0,0,1,2.78,1.887l-7.658,7.659a3.007,3.007,0,0,1-4.244,0L2.22,4.887A3,3,0,0,1,5,3ZM19,21H5a3,3,0,0,1-3-3V7.5L8.464,13.96a5.007,5.007,0,0,0,7.072,0L22,7.5V18A3,3,0,0,1,19,21Z"/>
-                        </svg>
-                        <input class="py-2 pl-12 pr-4 rounded-full w-[20rem]" type="text" name="send-email" id="send-email" placeholder="Enter your email address" value="{{ old('send-email') }}">
-                        </input>
-
-                        <input type="submit" name="send" id="name" placeholder="Subscibe to Newsletter">
-
-                    </form>
-                </div>
-            </div>
-
-            <div>
-
-            </div>
         </div>
     </footer>
 </body>
