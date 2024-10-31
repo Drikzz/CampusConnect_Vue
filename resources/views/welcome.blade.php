26a5f8a590738fad8243bbe2e6978e69b6d8d7ed
komi-cloud <qkiipubg@gmail.com>
2024-11-01 00:48:48 +0800

    changes on layout and landing
    - 1st commit on landing page
    - fixes to layout (since css overide)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
+    <div class="splash"> <!-- This is the div where the content will be displayed -->
+        <h1> WELCOME TO CAMPUS CONNECT!</h1>
+        <h2 class="splashtext">BROWSE CAMPUS<br>
+        CONNECT NOW.</h2>
+        <h3 class="smalltext">Browse through our diverse range of affordable student items here!</h3>
+    <br>
+        <button class="primary-btn"><a href="#">SHOP NOW</button>
+        
     
 </x-layout>
