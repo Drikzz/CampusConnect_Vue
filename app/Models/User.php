d5a57f1b457445d573992daaa1938a1f40cef30a
Zachary Labang <51441168+komorinn@users.noreply.github.com>
2025-02-10 20:17:57 +0800

    fixed user and added product page for admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -119,4 +119,10 @@ class User extends Authenticatable implements HasName
     {
         return $this->hasMany(Order::class, 'seller_code', 'seller_code'); // as seller
     }
+
+    public function isAdmin(): bool
+    {
+        return (bool) $this->is_admin;
+    }
+
 }
